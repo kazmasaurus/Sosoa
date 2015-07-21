cc60c2ebcddb01e11cd91b1eb2f674982b9c5be3
Zak Remer <zak.remer@gmail.com>
2015-07-21 16:07:00 -0700

    Creates Xcode project.

---
+++ Sosoa/Sosoa.h
@@ -0,0 +1,19 @@
+//
+//  Sosoa.h
+//  Sosoa
+//
+//  Created by Zak Remer on 7/21/15.
+//  Copyright (c) 2015 Zak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sosoa.
+FOUNDATION_EXPORT double SosoaVersionNumber;
+
+//! Project version string for Sosoa.
+FOUNDATION_EXPORT const unsigned char SosoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sosoa/PublicHeader.h>
+
+
