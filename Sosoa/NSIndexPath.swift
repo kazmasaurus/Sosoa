a71a95a5f763f20d173939030d7b8285a0658d9e
Zak Remer <zak.remer@gmail.com>
2015-09-22 16:55:20 -0700

    Removes outdated comment.

--- Sosoa/NSIndexPath.swift
+++ Sosoa/NSIndexPath.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-// This is only necessary in service of `Comparable`
-// Technically == is implemented by Apple, not in a way that consistently gives `Equatable`.
 extension NSIndexPath: Comparable { }
 public func < (lhs: NSIndexPath, rhs: NSIndexPath) -> Bool {
 	return lhs.compare(rhs) == .OrderedAscending
